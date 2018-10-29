b2300c8fad726a19ae89b0016af86f32aeabd9cd
Bartosz Kuriata <raximek@gmail.com>
2018-10-29 21:15:23 +0100

    PokerHands done

--- CodeWarsGeneral/Program.cs
+++ CodeWarsGeneral/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace CodeWarsGeneral
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            PokerHandTest("6S AD 7H 4S AS", "AH AC 5H 6H 7S");
+
+
+        }
+
+        public static void PokerHandTest(string hand, string opponentHand)
+        {
+            Result i = new PokerHand(hand).CompareWith(new PokerHand(opponentHand));
         }
     }
 }
