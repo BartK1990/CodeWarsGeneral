9ac2368222c460c907a4ea5ce647fd291708fcf9
Bartosz Kuriata <raximek@gmail.com>
2018-11-03 21:49:45 +0100

    Millionth Fibonacci

---
+++ CodeWarsGeneral/Fibonacci.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWarsGeneral
+{
+    using System.Numerics;
+
+    public class Fibonacci
+    {
+        public static BigInteger fib(int n)
+        {
+            if (n == 0) return BigInteger.Zero;
+            int nPositive = n;
+            if (n < 0) nPositive = 0 - n;
+            BigInteger fib1 = 1, fib1_temp; // for k = 1
+            BigInteger fib2 = 1, fib2_temp;
+            BigInteger fib_temp;
+            int k = 2;
+            bool whileBreaker = true;
+            bool direction = true;
+            while(whileBreaker)
+            {
+                if (k >= nPositive)
+                {
+                    if ((nPositive - (k/2)) <= (k - nPositive))
+                    {
+                        direction = true;
+                        break;
+                    }
+                    else
+                    {
+                        direction = false;
+                        whileBreaker = false;
+                    }
+                }
+                fib1_temp = fib1;
+                fib2_temp = fib2;
+                fib1 = fib1_temp * (2 * fib2_temp - fib1_temp);
+                fib2 = (fib2_temp * fib2_temp) + (fib1_temp * fib1_temp);
+                k *= 2;
+            }
+            k = (k / 2);
+            if (k == nPositive)
+                return ((nPositive % 2) == 0) ? 0-fib1 : fib1;
+            k = direction ? k+1 : k;
+            if (direction)
+            {
+                for (; k < nPositive; k++)
+                {
+                    fib_temp = fib2;
+                    fib2 = fib1 + fib2;
+                    fib1 = fib_temp;
+                }
+            }
+            else
+            {
+                for (; k > nPositive; k--)
+                {
+                    fib_temp = fib1;
+                    fib1 = fib2 - fib1;
+                    fib2 = fib_temp;
+                }
+            }     
+            if(((nPositive % 2) == 0) && (n<0)) // for negatives
+                return direction ? 0-fib2 : 0-fib1;
+            else
+                return direction ? fib2 : fib1;
+        }
+    }
+}
--- CodeWarsGeneral/Program.cs
+++ CodeWarsGeneral/Program.cs
@@ -10,14 +10,33 @@ namespace CodeWarsGeneral
     {
         static void Main(string[] args)
         {
-            PokerHandTest("6S AD 7H 4S AS", "AH AC 5H 6H 7S");
-
-
+            /*
+            Console.WriteLine(Fibonacci.fib(0));
+            Console.WriteLine(Fibonacci.fib(1));
+            Console.WriteLine(Fibonacci.fib(2));
+            Console.WriteLine(Fibonacci.fib(3));
+            Console.WriteLine(Fibonacci.fib(4));
+            Console.WriteLine(Fibonacci.fib(5));
+            Console.WriteLine(Fibonacci.fib(6));
+            Console.WriteLine(Fibonacci.fib(7));
+            Console.WriteLine(Fibonacci.fib(8));
+            Console.WriteLine(Fibonacci.fib(9));
+            Console.WriteLine(Fibonacci.fib(10));
+            Console.WriteLine(Fibonacci.fib(11));
+            Console.WriteLine(Fibonacci.fib(12));
+            Console.WriteLine(Fibonacci.fib(13));
+            Console.WriteLine(Fibonacci.fib(14));
+            */
+            Console.WriteLine(Fibonacci.fib(-1));
+            Console.WriteLine(Fibonacci.fib(-2));
+            Console.WriteLine(Fibonacci.fib(-3));
+            Console.WriteLine(Fibonacci.fib(-4));
+            Console.WriteLine(Fibonacci.fib(-5));
+            Console.WriteLine(Fibonacci.fib(-6));
+            Console.WriteLine(Fibonacci.fib(-7));
+            Console.ReadKey();
         }
 
-        public static void PokerHandTest(string hand, string opponentHand)
-        {
-            Result i = new PokerHand(hand).CompareWith(new PokerHand(opponentHand));
-        }
+
     }
 }
